ba13313246a9c1fca0e1f2e0e40d013194858772
Jill.Ward <jill.ward@ft-mw5134.osb.ft.com>
2019-09-11 16:40:59 +0100

    Write additional timestamp field to dynamoDB

--- dynamo.js
+++ dynamo.js
@@ -1,33 +1,38 @@
+const moment = require('moment');
 const AWS = require('aws-sdk');
 const uuid = require('uuidv4').default;
 
 module.exports = {
-    putItem: () => {
-    const dynamodb = new AWS.DynamoDB.DocumentClient();
-    var params = {
-     Item: {
-        'approval-id': `aid-${uuid()}`,
-        timestamp: '23/08/2019 14:33:21',
-        'requester-email': 'jill.ward@ft.com',
-        'request-type': 'Conference',
-        'request-url': 'http://www.conferencename.com',
-        'cost': '500',
-        'calendar-year': '2019',
-        'travel-cost': '0',
-        'addl-info': 'Additional info here'         
-    }, 
-    TableName: "submit-approvals"
-   };
-        console.log("Put item into DynamoDB");
-        console.log(params.Item['approvalId'])
-           dynamodb.put(params, function(err, data) {
-             if (err) {
-                console.log(err, err.stack); // an error occurred
-             } else {
-                console.log('Item was put successfully');           // successful response
-             }    
-           });
-    }
+    putItem: (request) => {
+      const dynamodb = new AWS.DynamoDB.DocumentClient();
+      var params = {
+         Item: {
+            'approval-id': `aid-${uuid()}`,
+            'user': request.user,
+            'reason': request.reason,
+            'url': request.url,
+            'cost': request.cost,
+            'calendarYear': request.calendarYear,
+            'travelCost': request.travelCost,
+            'additionalInfo': request.additionlInfo,
+            'status': request.status, 
+            'requestDate': request.requestDate,   
+            'actionedDate': moment().format("DD/MM/YYYY HH:mm:ss")   
+         }, 
+         TableName: "submit-approvals"
+      };
+
+      dynamodb.put(params, function(err, data) {
+         if (err) {
+            console.log(err, err.stack); // an error occurred
+         } else {
+            console.log('Item was put successfully'); // successful response
+         }    
+      });  
+      // To update the dynamoDB item to determine whether or not a request has been approved or denied, see 
+      // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB.html#updateItem-property
+      // Note: Function is labelled as updateItem but may actually be update.
+   }
 }
 
 
--- index.js
+++ index.js
@@ -4,13 +4,8 @@ exports.handler = async (event) => {
   const decodedMessage = decodeURIComponent(event.body);
   const messageObjectString = decodedMessage.split('payload=')[1]
   const messageObject = JSON.parse(messageObjectString)
-  const messageValue = messageObject.actions[0].value
-  
-  if(messageValue === 'Approve') {
-      console.log('This has been approved!!')
-      putItem();
-      
-  } else {
-      console.log('Denied!!!')
-  }
+  const messageValue = JSON.parse(messageObject.actions[0].value)
+
+  putItem(messageValue);
+
 }
