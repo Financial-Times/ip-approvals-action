e5c30383051425bdbc571f37cb9583084d5511db
Jill.Ward <jill.ward@ft-mw5134.osb.ft.com>
2019-09-11 16:55:03 +0100

    If statement in index js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const {putItem} = require('./dynamo');
 
 exports.handler = async (event) => {
-  const decodedMessage = decodeURIComponent(event.body);
-  const messageObjectString = decodedMessage.split('payload=')[1]
-  const messageObject = JSON.parse(messageObjectString)
-  const messageValue = JSON.parse(messageObject.actions[0].value)
+  if(event) {
+    const decodedMessage = decodeURIComponent(event.body);
+    const messageObjectString = decodedMessage.split('payload=')[1]
+    const messageObject = JSON.parse(messageObjectString)
+    const messageValue = JSON.parse(messageObject.actions[0].value)
 
-  putItem(messageValue);
+    putItem(messageValue);
+  } else {
+    console.log("No event")
+    // Use this to retrigger a slack message to approver 
+  }
 
 }
